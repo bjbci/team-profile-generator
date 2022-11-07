5252a95943f2ab982b7706d8de9fd52ee3b9f07e
Brian Bulowicz <bajebi.bb@gmail.com>
2022-11-07 00:24:07 -0600

    just getting started commit 2

---
+++ Employee.js
@@ -0,0 +1,120 @@
+//const  employee=require('./Employee')
+function Employee(name,id,email){
+// The first class is an `Employee` parent class with the following properties and methods:
+
+// * `name`
+
+// * `id`
+
+// * `email`
+
+// * `getName()`
+
+// * `getId()`
+
+// * `getEmail()`
+
+// * `getRole()`&mdash;returns `'Employee'`
+
+// The other three classes will extend `Employee`.
+}
+
+Employee.prototype.getName(){}
+Employee.prototype.getId(){}
+Employee.prototype.getEmail(){}
+module.exports=Employee;
+
+
+//const  engineer=require('./Employee')
+engineer(){
+// The first class is an `Employee` parent class with the following properties and methods:
+
+// * `name`
+
+// * `id`
+
+// * `email`
+
+// * `getName()`
+
+// * `getId()`
+
+// * `getEmail()`
+
+// * `getRole()`&mdash;returns `'Employee'`
+
+// The other three classes will extend `Employee`.
+
+// In addition to `Employee`'s properties and methods, `Engineer` will also have the following:
+
+// * `github`&mdash;GitHub username
+
+// * `getGithub()`
+
+// * `getRole()`&mdash;overridden to return `'Engineer'`
+}
+module.exports=engineer;
+
+
+
+//const intern=require('./Employee')
+intern (){
+// The first class is an `Employee` parent class with the following properties and methods:
+
+// * `name`
+
+// * `id`
+
+// * `email`
+
+// * `getName()`
+
+// * `getId()`
+
+// * `getEmail()`
+
+// * `getRole()`&mdash;returns `'Employee'`
+
+// The other three classes will extend `Employee`.
+
+
+// In addition to `Employee`'s properties and methods, `Intern` will also have the following:
+
+// * `school`
+
+// * `getSchool()`
+
+// * `getRole()`&mdash;overridden to return `'Intern'`
+}
+module.exports=intern;
+
+
+
+//const manager=require('./Employee')
+manager(){
+// The first class is an `Employee` parent class with the following properties and methods:
+
+// * `name`
+
+// * `id`
+
+// * `email`
+
+// * `getName()`
+
+// * `getId()`
+
+// * `getEmail()`
+
+// * `getRole()`&mdash;returns `'Employee'`
+
+// The other three classes will extend `Employee`.
+
+
+// In addition to `Employee`'s properties and methods, `Manager` will also have the following:
+
+// * `officeNumber`
+
+// * `getRole()`&mdash;overridden to return `'Manager'`
+}
+module.exports=manager;
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,3 +1,59 @@
+const Employee=require('../Employee.js')
+
+describe("Employee constructor", ()=>{
+    it("returns an Employee object", ()=>{
+        //arrainge
+        const name="Brian"
+        const id=6666
+        const email="bajebi@mac.com"
+        //act
+        const employee=new Employee(name,id,email)
+        console.log(employee)
+
+        //assert
+        expect(employee instanceof Employee).toBe(true)
+
+    })
+
+    it("has a name", ()=>{
+        const name="Brian"
+        const id=6666
+        const email="bajebi@mac.com" 
+            //act
+        const employee=new Employee(name,id,email)
+ 
+         //assert
+        expect(employee.name).toBe(name)
+        
+    })
+
+    it("has an Id", ()=>{
+        const name="Brian"
+        const id=6666
+        const email="bajebi@mac.com"
+         //act
+         const employee=new Employee(name,id,email)
+
+        //assert
+        expect(employee.id).toBe(true)
+    })
+
+    it("has an email", ()=>{
+        const name="Brian"
+        const id=6666
+        const email="bajebi@mac.com"
+         //act
+        const employee=new Employee(name,id,email)
+
+        //assert
+        expect(employee.email).toBe(true)
+    })
+
+
+
+
+})
+
 // The first class is an `Employee` parent class with the following properties and methods:
 
 // * `name`
@@ -15,3 +71,4 @@
 // * `getRole()`&mdash;returns `'Employee'`
 
 // The other three classes will extend `Employee`.
+
