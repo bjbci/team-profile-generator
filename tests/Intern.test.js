ccc0424cd58379446c786f7882bf2beb019b704e
Brian Bulowicz <bajebi.bb@gmail.com>
2022-11-06 20:39:45 -0600

    first commit

---
+++ tests/Intern.test.js
@@ -0,0 +1,27 @@
+// The application must include `Employee`, `Manager`, `Engineer`, and `Intern` classes. The tests for these classes (in the `_tests_` directory) must ALL pass.
+
+// The first class is an `Employee` parent class with the following properties and methods:
+
+// * `name`
+
+// * `id`
+
+// * `email`
+
+// * `getName()`
+
+// * `getId()`
+
+// * `getEmail()`
+
+// * `getRole()`&mdash;returns `'Employee'`
+
+// The other three classes will extend `Employee`.
+
+// In addition to `Employee`'s properties and methods, `Intern` will also have the following:
+
+// * `school`
+
+// * `getSchool()`
+
+// * `getRole()`&mdash;overridden to return `'Intern'`
